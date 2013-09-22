863af17cef879cb42c475194a495f9f8d8fc1501
Anna Henningsen <sqrt@entless.org>
2013-09-22 15:08:30 +0000

    bugfix

--- objects.js
+++ objects.js
@@ -67,7 +67,7 @@ DBSubsystemBase.prototype.feed = function(data) {
 }
 
 DBSubsystemBase.prototype.fetchEvents = function(query, user, access, cb) {
-	this.query('SELECT events.*, events_users.*, c.*, oh.*, events.time AS eventtime, events.eventid AS eventid, su.name, trader.id AS traderid, trader.name AS tradername, AS srcusername FROM events_users '+
+	this.query('SELECT events.*, events_users.*, c.*, oh.*, events.time AS eventtime, events.eventid AS eventid, trader.id AS traderid, trader.name AS tradername, su.name AS srcusername FROM events_users '+
 		'JOIN events ON events_users.eventid = events.eventid '+
 		'LEFT JOIN ecomments AS c ON c.commentid = events.targetid AND events.type="comment" '+
 		'LEFT JOIN events AS e2 ON c.eventid=e2.eventid AND e2.type="trade" '+

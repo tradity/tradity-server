1594eeb5cf01102e99c9b97364decd9432d50232
Anna Henningsen <sqrt@entless.org>
2014-02-15 11:34:33 +0000

    re-fix

--- user.js
+++ user.js
@@ -122,7 +122,7 @@ UserDB.prototype.getRanking = function(query, user, access, cb_) {
 			'LEFT JOIN schoolmembers AS sm ON users.id = sm.uid ' +
 			'LEFT JOIN schools ON sm.schoolid = schools.id ';
 		
-		this.query('SELECT rank, users.id AS uid, users.name AS name, schools.path AS schoolpath, schools.id AS school, schools.name AS schoolname, jointime, ' +
+		this.query('SELECT rank, users.id AS uid, users.name AS name, schools.path AS schoolpath, schools.id AS school, schools.name AS schoolname, jointime, pending, ' +
 			'totalvalue, weekstarttotalvalue, weekstartprov_sum, wprov_sum + lprov_sum AS prov_sum, tradecount != 0 as hastraded, ' +
 			'(dayfperfcur+weekfperfsold) / weekfperfbase AS weekfperf, (dayfperfcur+totalfperfsold) / totalfperfbase AS totalfperf, ' +
 			'(dayfperfcur+totalfperfsold-totalfperfbase)/GREATEST(700000000, totalvalue) AS totalfperfval, (dayfperfcur+weekfperfsold-weekfperfbase)/GREATEST(700000000, weekstarttotalvalue) AS weekfperfval, ' +

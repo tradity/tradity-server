084abb33b9f697fb1494fd954a27ce3b132a36b8
Anna Henningsen <sqrt@entless.org>
2014-09-19 01:09:45 +0200

    add qcontext provider

---
+++ qctx.js
@@ -0,0 +1,28 @@
+(function () { "use strict";
+
+var Access = require('./access.js').Access;
+
+var QContext = function(obj) {
+	obj = obj || {};
+	this.user = obj.user || null;
+	this.access = obj.access || new Access();
+};
+
+QContext.prototype.toJSON = function() {
+	return { user: this.user, access: this.access };
+};
+
+exports.fromJSON =
+QContext.fromJSON = function(j) {
+	var ctx = new QContext();
+	if (!j)
+		return ctx;
+	
+	ctx.user = j.user || null;
+	ctx.access = Access.fromJSON(access);
+	return ctx;
+};
+
+exports.QContext = QContext;
+
+})();

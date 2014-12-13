32429ecdc51ea820cdeb077ed34544fe3cdb6722
Anna Henningsen <sqrt@entless.org>
2014-12-13 11:19:39 +0100

    fix comment typo

--- bus/bus.js
+++ bus/bus.js
@@ -406,7 +406,7 @@ Bus.prototype.handleBusPacket = function(packet) {
 		assert.ok(_.isString(recpId));
 		
 		if (recpId == self.id) {
-			// defer handling, since we might have received a message which invalides the bus graph
+			// defer handling, since we might have received a message which invalidates the bus graph
 			packetIsForSelf = true;
 		} else {
 			var targetNode = self.busGraph.getElementById(recpId);

978054679450d4964e418879b1b3196745753798
Anna Henningsen <sqrt@entless.org>
2015-01-29 03:28:59 +0100

    iQIbBAABAgAGBQJWis71AAoJENi59a6uhOTPt80P+Kyib5Hf7FKllVIAnWU2E+j0  G42SZuaJyQtgK30ISTsJoL/QAPlQPozfp+yOgc7d/KAPpEbICnizDKefoRFuH0S7  h+9vkxe11LQG4sjzgoD28jlf7wG9bsBrjxxJLPWWeS2WrJn1XsH3HiV/akYX9tsy  8Js5yZTpE3WqFeXke9Y64+9kh0QUUSFbc/BTcQrX0v+xZLAjsLnbC7InI4NBJq/q  r1nR8StvO6vhvVu7U2vc60lp+jpzBu/SokuKwXTD0aW4Zq8XuUPaFAdOaP/Yv1D+  LSqxzJfSB2Fno3ht2q9Y0uANs4j7vnOQfSqx3DNg7WOwZjQLTxpa9PVdEpu1f3a4  B82UViCwnq47qkWnJPcw2QBBaYobzhM/9RW5Kx5gh4urlKW4N6xXBcldbDtcdbZ3  cIYxpJkf9Ny4ha3HITOlS698CxcI5r2KdY0fvLKcqRNG4qzm8G/F2eAeXzscs96S  L9DARs3Ou0JclYq8bYrcjqFNb3kCfm0bDKoDxBW4vI1Xbm84HduhF3oLOx/gBUim  YUE4O5EbdgUEB3sSiSG5Wl1Z+qQancrzT+X/LZcZBM66tY9ITN/VIZW+14NlnWQ8  SynAkDBiqdmgpmBUVpU4OIA8VPSoHkVZYGTl09591xo6lKgeJCCK9ZlAF56tS7qT  X85hrH3cu4WpGVgQlME=  =prrl  -----END PGP SIGNATURE-----
    Add test stub for wordpress-feed.js

---
+++ test/wordpress-feed.js
@@ -0,0 +1,22 @@
+var assert = require('assert');
+var testHelpers = require('./test-helpers.js');
+var socket;
+
+before(function() {
+	return testHelpers.standardSetup().then(function(data) {
+		socket = data.socket;
+	});
+});
+
+beforeEach(testHelpers.standardReset);
+after(testHelpers.standardTeardown);
+
+describe('wordpress-feed', function() {
+	describe('process-wordpress-feed', function() {
+		it('Should work', function() {
+			return socket.emit('process-wordpress-feed').then(function(res) {
+				assert.equal(res.code, 'process-wordpress-feed-success');
+			});
+		});
+	});
+});

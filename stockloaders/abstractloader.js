69514f4b83f6cd4ca9b165a3834532ee2ef2986c
Anna Henningsen <sqrt@entless.org>
2015-04-17 18:00:22 +0200

    Re-write stock quote loaders in promise-based style

--- stockloaders/abstractloader.js
+++ stockloaders/abstractloader.js
@@ -22,16 +22,19 @@ AbstractLoader.prototype._handleRecord = function(record, cached) {
 		this.cache['s-' + record.symbol] = record;
 	
 	this.emit('record', record);
+	
+	return record;
 };
 
 AbstractLoader.prototype._makeQuoteRequest = function(stocklist) {
 	var self = this;
 	
+	var cachedResults = [];
 	stocklist = _.filter(stocklist, function(stockid) {
 		var cv = self.cache['s-' + stockid];
 		if (cv) {
 			if (cv.fetchTime > Date.now() - self.cacheTime) {
-				self._handleRecord(cv, true);
+				cachedResults.push(self._handleRecord(cv, true));
 				return false;
 			} else {
 				delete self.cache['s-' + stockid];
@@ -41,81 +44,32 @@ AbstractLoader.prototype._makeQuoteRequest = function(stocklist) {
 		return true;
 	});
 	
-	if (stocklist.length > self.maxlen) {
-		self._makeQuoteRequest(stocklist.slice(self.maxlen));
-		self._makeQuoteRequest(stocklist.slice(0, self.maxlen));
-		return;
-	}
-	
 	if (stocklist.length == 0) // everything was cached
-		return;
+		return Q(cachedResults);
 	
-	return self._makeQuoteRequestFetch(stocklist);
+	// split stocklist into groups of maximum length maxlen
+	// and flatten the resulting chunked array of records
+	var chunkedStocklist = _.chunk(stocklist, self.maxlen);
+	
+	return Q.all(chunkedStocklist.map(function(chunk) {
+		return self._makeQuoteRequestFetch(chunk);
+	})).then(function(recordListChunks) {
+		return _.flatten(recordListChunks).concat(cachedResults).filter(function(record) {
+			return !record.failure;
+		});
+	});
 };
 
-AbstractLoader.prototype.loadQuotes = function(stocklist, filter, callback) {
+AbstractLoader.prototype.loadQuotesList = function(stocklist, filter) {
 	var self = this;
 	
-	filter = filter || function() { return true; };
-	callback = callback || function() {};
+	filter = filter || _.constant(true);
 	
-	if (stocklist.length == 0) {
-		self.emit('error', new Error('Called with empty stocklist'));
-		return;
-	}
-	
-	_.each(stocklist, function(e) {
-		var cb = function(record) {
-			// .indexOf uses strict comparison
-			if ([record.isin, record.wkn, record.failure].map(String).indexOf(String(e)) != -1 && filter(record)) {
-				self.removeListener('record', cb);
-				callback(record);
-			}
-		};
-		
-		self.on('record', cb);
+	return Q().then(function() {
+		return self._makeQuoteRequest(stocklist);
+	}).then(function(records) {
+		return _.filter(records, filter);
 	});
-	
-	self._makeQuoteRequest(stocklist);
-};
-
-AbstractLoader.prototype.loadQuotesList = function(stocklist, filter, callback) {
-	filter = filter || function() { return true; };
-	callback = callback || function() {};
-	
-	var resultNames = [];
-	var results = [];
-	
-	if (stocklist.length == 0) {
-		callback([]);
-		return Q([]);
-	}
-	
-	var deferred = Q.defer();
-	this.loadQuotes(stocklist, null, function(rec) {
-		if (results === null)
-			return;
-		
-		if (rec.isin) {
-			resultNames.push(rec.isin);
-			resultNames.push(rec.wkn);
-			resultNames.push(rec.name);
-			results.push(rec);
-		}
-		
-		if (rec.failure)
-			resultNames.push(rec.failure);
-		
-		if (_.difference(stocklist, resultNames).length == 0) {
-			var filteredResults = _.filter(results, filter);
-			
-			deferred.resolve(filteredResults);
-			callback(filteredResults);
-			results = null;
-		}
-	});
-	
-	return deferred.promise;
 };
 
 exports.AbstractLoader = AbstractLoader;

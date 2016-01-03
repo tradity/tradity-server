6aaf905813a4ec77477319f4dd70ead6858c9063
Anna Henningsen <sqrt@entless.org>
2016-01-03 20:49:20 +0100

    Use jsdoc version in node_modules/

--- generate_docs.sh
+++ generate_docs.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-jsdoc *.js bus/*.js stockloaders/*.js lib/*.js node_modules/tradity-connection/*.js README.md --destination ./doc/
+./node_modules/.bin/jsdoc *.js stockloaders/*.js lib/*.js \
+  node_modules/tradity-connection/*.js \
+  README.md --destination ./doc/ \
+  || true

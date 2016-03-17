7b1ed86e6111ca4b4b59936d8a4d996520b62559
Anna Henningsen <sqrt@entless.org>
2016-03-17 06:28:03 +0100

    Add wordpress integration test using nock

--- test/blackbox/wordpress-feed.js
+++ test/blackbox/wordpress-feed.js
@@ -17,12 +17,69 @@
 'use strict';
 
 const assert = require('assert');
+const nock = require('nock');
 const testHelpers = require('./test-helpers.js');
 
 describe('wordpress-feed', function() {
   let socket, user;
   
   before(function() {
+    
+    nock('https://example.com')
+      .persist()
+      .get(/\/posts$/)
+      .reply(200, [
+        {
+          "link": "https://example.com/somewhere/",
+          "modified": "2016-03-14T08:50:56",
+          "parent": null,
+          "modified_tz": "Etc/GMT+2",
+          "comment_status": "open",
+          "author": {
+            "URL": "",
+            "name": "authorx",
+            "description": "",
+            "registered": "2014-05-29T15:43:42+00:00",
+            "ID": 7,
+            "username": "authorx",
+            "nickname": "authorx",
+            "first_name": "authorx",
+            "slug": "moritz"
+          },
+          "menu_order": 0,
+          "excerpt": "<p>Excerpt</p>\n",
+          "status": "publish",
+          "date": "2016-03-13T23:50:37",
+          "ping_status": "open",
+          "type": "post",
+          "sticky": false,
+          "title": "Some title",
+          "ID": 3855,
+          "terms": {
+            "category": [
+              {
+                "parent": null,
+                "count": 15,
+                "link": "https://example.com/uncategorized/",
+                "slug": "uncategorized",
+                "taxonomy": "category",
+                "name": "Uncategorized",
+                "description": "",
+                "ID": 1
+              }
+            ]
+          },
+          "format": "standard",
+          "slug": "authorx-wins-it-all",
+          "content": "<h1>Content!</h1>\n",
+          "modified_gmt": "2016-03-14T06:50:56",
+          "guid": "https://example.com/?p=3855",
+          "date_tz": "Etc/GMT+2",
+          "date_gmt": "2016-03-13T21:50:37",
+          "featured_image": null
+        }
+      ]);
+    
     return testHelpers.standardSetup().then(data => {
       socket = data.socket;
       user = data.user;
@@ -44,7 +101,8 @@ describe('wordpress-feed', function() {
   
   describe('/wordpress/addFeed', function() {
     it('Should add a wordpress feed entry which can later be removed', function() {
-      const endpoint = 'https://example.com/' + Date.now();
+      const path = Date.now();
+      const endpoint = 'https://example.com/' + path;
       
       return socket.post('/wordpress/addFeed', {
         __sign__: true,
@@ -54,6 +112,13 @@ describe('wordpress-feed', function() {
           schoolid: null,
           bloguser: user.uid
         }
+      }).then(res => {
+        assert.ok(res._success);
+        
+        return socket.post('/wordpress/processFeed', {
+          __sign__: true
+        });
+        
       }).then(res => {
         assert.ok(res._success);
         
